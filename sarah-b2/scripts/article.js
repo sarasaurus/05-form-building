195ca62d92595da61286a959bb70e0aaf226ef6d
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-10 18:27:52 -0800

    ARticle.js tohtml method refactored, into non arrow function, articles load now

--- sarah-b2/scripts/article.js
+++ sarah-b2/scripts/article.js
@@ -12,7 +12,7 @@ function Article (rawDataObj) {
   //articles.push(this);
 }
 
-Article.prototype.toHtml = () => {
+Article.prototype.toHtml = function () {
   let template = Handlebars.compile($('#article-template').text());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
@@ -25,8 +25,8 @@ Article.prototype.toHtml = () => {
 
 // REVIEW: We can write an arrow function in one line, without the code block, making the code easier to read. With an arrow function, the 'return' is implicit, we can remove it as well.
 // The .sort() method will rearrange the order of elements in the array and return the original array. This method does not make a copy. See the MDN docs for more details.
-rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
+rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
 
-rawData.forEach(articleObject => articles.push(new Article(articleObject)))
+rawData.forEach(articleObject => articles.push(new Article(articleObject)));
 
-articles.forEach(article => $('#articles').append(article.toHtml()))
+articles.forEach(article => $('#articles').append(article.toHtml()));
--- sarah-b2/scripts/articleView.js
+++ sarah-b2/scripts/articleView.js
@@ -77,13 +77,12 @@ articleView.setTeasers = () => {
 // COMMENT: Where is this function called? Why?
 // this will be called on the new article page, so that different scripts will run on different pages
 articleView.initNewArticlePage = () => {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-
-
-  // TODO: The new articles we create will be copy/pasted into our source data file.
+  // DONE: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  $('.tab-content').show();
+  // DONE: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
-
-  $('#article-json').on('focus', function(){
+  $('#article-export').hide();
+  $('#json-export').on('focus', function(){
     this.select();
   });
 
